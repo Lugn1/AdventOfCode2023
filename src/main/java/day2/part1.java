94061b0739fc6ca6ab0b0c328b22603ff8387421
William Karlström <wkarlstrom@outlook.com>
2023-12-06 16:27:16 +0100

    Complete day 2

--- src/main/java/day2/part1.java
+++ src/main/java/day2/part1.java
@@ -8,6 +8,7 @@ public class part1 {
     public static void main(String[] args) {
         String puzzleInput = "./src/main/java/day2/puzzle_input";
         int sumOfGameIDs = 0;
+        int sumOfPowers = 0;
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(puzzleInput));
@@ -17,11 +18,14 @@ public class part1 {
                 if (checkPossibility(line)) {
                     sumOfGameIDs += gameId;
                 }
+                Cubes cubes = cubesAndPower(line);
+                sumOfPowers += cubes.getPower();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         System.out.println("Sum of IDs for possible games: " + sumOfGameIDs);
+        System.out.println("Sum of powers: " + sumOfPowers);
     }
 
     public static boolean checkPossibility(String input) {
@@ -51,9 +55,53 @@ public class part1 {
         }
         return true;
     }
+
+    // Part 2
+    static class Cubes {
+        int red;
+        int green;
+        int blue;
+
+        public Cubes(int red, int green, int blue) {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+
+        int getPower() {
+            return red * green * blue;
+        }
+    }
+
+
+    public static Cubes cubesAndPower(String input) {
+        int highestRed = 0;
+        int highestGreen = 0;
+        int highestBlue = 0;
+        String gameData = input.substring(input.indexOf(':') + 1).trim();
+        String[] parts = gameData.split(";");
+
+        for (String part : parts) {
+            int localRed = 0, localGreen = 0, localBlue = 0;
+            String[] gems = part.split(",");
+            for (String gem : gems) {
+                gem = gem.trim();
+                String[] gemDetails = gem.split(" ");
+                if (gemDetails.length == 2) {
+                    int count = Integer.parseInt(gemDetails[0]);
+                    String color = gemDetails[1].toLowerCase();
+
+                    if (color.equals("red")) localRed = count;
+                    if (color.equals("green")) localGreen = count;
+                    if (color.equals("blue")) localBlue = count;
+
+                    highestRed = Math.max(highestRed, localRed);
+                    highestGreen = Math.max(highestGreen, localGreen);
+                    highestBlue = Math.max(highestBlue, localBlue);
+                }
+            }
+        }
+        return new Cubes(highestRed, highestGreen, highestBlue);
+    }
 }
-
-
-
-
 
