b27ca2138562d9c8095f156cfc96778c53e9b563
William Karlström <wkarlstrom@outlook.com>
2023-12-07 16:46:10 +0100

    Complete day 3 part 1

--- src/main/java/day3/day3.java
+++ src/main/java/day3/day3.java
@@ -4,12 +4,14 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class day3 {
 
     public static void main(String[] args) {
-        String fileName = "./src/main/java/day3/sample_input";
+        String fileName = "./src/main/java/day3/puzzle_input";
         List<String> schematic = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
@@ -25,29 +27,37 @@ public class day3 {
 
     public static int sumPartNumbers(List<String> schematic) {
         int sum = 0;
+        Set<String> countedNumbers = new HashSet<>();
         for (int i = 0; i < schematic.size(); i++) {
             String line = schematic.get(i);
             for (int j = 0; j < line.length(); j++) {
-                if (Character.isDigit(line.charAt(j)) && isAdjacentToSymbol(schematic, i, j)) {
+                if (Character.isDigit(line.charAt(j)) && !countedNumbers.contains(i + "," + j)) {
                     int[] numberInfo = extractNumber(schematic, i, j);
-                    sum += numberInfo[0];
-                    j = j + numberInfo[1] - 1; // Set j to the last digit of the current number
+                    if (isAdjacentToSymbol(schematic, i, j, numberInfo[1])) {
+                        sum += numberInfo[0];
+                        for (int k = 0; k < numberInfo[1]; k++) {
+                            countedNumbers.add(i + "," + (j + k));
+                        }
+                    }
+                    j += numberInfo[1] - 1;
                 }
             }
         }
         return sum;
     }
 
-    private static boolean isAdjacentToSymbol(List<String> schematic, int i, int j) {
-        for (int row = i - 1; row <= i + 1; row++) {
-            for (int col = j - 1; col <= j + 1; col++) {
-                if (row >= 0 && row < schematic.size() && col >= 0 && col < schematic.get(row).length()) {
-                    if (row == i && col == j) {
-                        continue; // skip the number itself
-                    }
-                    char c = schematic.get(row).charAt(col);
-                    if (c != '.' && !Character.isDigit(c)) {
-                        return true;
+    private static boolean isAdjacentToSymbol(List<String> schematic, int i, int startJ, int length) {
+        for (int j = startJ; j < startJ + length; j++) {
+            for (int row = i - 1; row <= i + 1; row++) {
+                for (int col = j - 1; col <= j + 1; col++) {
+                    if (row >= 0 && row < schematic.size() && col >= 0 && col < schematic.get(row).length()) {
+                        if (row == i && col == j) {
+                            continue;
+                        }
+                        char c = schematic.get(row).charAt(col);
+                        if (c != '.' && !Character.isDigit(c)) {
+                            return true;
+                        }
                     }
                 }
             }
@@ -64,6 +74,7 @@ public class day3 {
             j++;
             length++;
         }
-        return new int[]{Integer.parseInt(number.toString()), length};
+        int num = Integer.parseInt(number.toString());
+        return new int[]{num, length};
     }
 }
